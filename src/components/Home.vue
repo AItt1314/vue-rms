0fa89902509dc1e05c4cf7bd9f0da4b6767e88d7
aitt <2368724272@qq.com>
2022-05-06 22:09:23 +0800

    完成了左侧侧边栏的路由改造

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -12,38 +12,48 @@
     </el-header>
     <el-container>
       <!-- 左侧侧边栏 -->
-      <el-aside width="200px">
+      <!-- 根据是否折叠来决定侧边栏的宽度 -->
+      <el-aside :width="collapse.isCollapse ? '64px' : '200px'">
+          <!-- 折叠按钮 -->
+          <div class="el-icon-s-operation toggle_btn" @click="toggle"></div>
           <!-- 大菜单 -->
         <el-menu
-          default-active="2"
           class="el-menu-vertical-demo"
-          @open="handleOpen"
-          @close="handleClose"
           background-color='#ecdef0'
           text-color='#590505'
+          active-text-color='#2000ff' 
+          :unique-opened=true
+          :collapse='collapse.isCollapse'
+          :collapse-transition = false
+          :router=true
         >
         <!-- 一级菜单 -->
-          <el-submenu>
+        <!-- 每一个一级菜单都应该有自己独一无二的index，否则点击按钮会导致所有菜单同步 -->
+          <el-submenu v-for='item in MenuList' :key='item.id' :index='"/"+item.path'>
               <!-- 一级菜单模板区 -->
             <template slot="title">
                 <!-- 一级菜单图标 -->
-              <i class="el-icon-location"></i>
+                <!-- 动态绑定，obj[index] 表示对象中键为index的键值 -->
+              <i :class="iconsObj[item.id]"></i>
               <!-- 一级菜标题名 -->
-              <span>导航一</span>
+              <span>{{item.authName}}</span>
             </template>
             <!-- 二级菜单 -->
-            <el-menu-item index="1-4-1"><template slot="title">
+            <el-menu-item :index="'/'+subitem.path" v-for="subitem in item.children" :key='subitem.id'><template slot="title">
                 <!-- 二级菜单图标 -->
-              <i class="el-icon-location"></i>
+              <i class="el-icon-menu"></i>
               <!-- 二级菜标题名 -->
-              <span>导航一</span>
+              <span>{{subitem.authName}}</span>
             </template></el-menu-item>
           </el-submenu>
           
         </el-menu></el-aside
       >
       <!-- 主要内容栏 -->
-      <el-main>Main</el-main>
+      <el-main>
+          <!-- 路由占位符 -->
+          <router-view></router-view>
+      </el-main>
     </el-container>
   </el-container>
 </template>
@@ -51,7 +61,22 @@
 <script>
 export default {
   data() {
-    return {};
+    return {
+        //ajax 获取的数据
+        MenuList:[],
+        // 为不同的一级菜单添加不同的图标
+        iconsObj:{
+            125:'el-icon-user',
+            103:'el-icon-s-tools',
+            101:'el-icon-sell',
+            102:'el-icon-s-ticket',
+            145:'el-icon-postcard'
+        },
+        //是否折叠，及折叠后的侧边栏宽度
+        collapse:{
+            isCollapse: false,
+            }
+    };
   },
   methods: {
     //退出登录事务：1.清空存储的token window.sessionStorage.clear() 2.跳转至登录页 this.$router.push('/login')
@@ -59,7 +84,19 @@ export default {
       window.sessionStorage.clear();
       this.$router.push("/login");
     },
+    async getMenuList(){
+        const {data : res} = await this.$http.get('menus');
+        if(res.meta.status!==200) return  this.$message.error(res.meta.msg);
+        this.MenuList = res.data;
+    },
+    toggle(){
+        this.collapse.isCollapse = !this.collapse.isCollapse
+    }
   },
+//   获取左侧所有菜单
+  created () {
+      this.getMenuList()
+  }
 };
 </script>
 
@@ -83,6 +120,20 @@ export default {
   font-size: 25px;
   color: #fff;
 }
+/* 折叠按钮 */
+.toggle_btn {
+    position: relative;
+    width: 100%;
+    height: 30px;
+    cursor: pointer;
+    background-color: #ced7e7;
+}
+.el-icon-s-operation:before {
+    position: absolute;
+    right: 0px;
+    font-size: 30px;
+    text-align: center;
+}
 /* 左侧侧边栏区域 */
 .el-aside {
   background-color: #ecdef0;
@@ -93,6 +144,13 @@ export default {
 .el-submenu__title span{
     font-size: 18px;
 }
+.el-submenu [class^=el-icon-] {
+    margin-right: 15px;
+}
+
+
+
+
 .el-main {
   background-color: #f3e7e7;
 }
